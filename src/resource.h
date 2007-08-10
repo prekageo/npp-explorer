6dc3291905e12abd5f8deba50f0b68f8406c419c
jenslorenz <jenslorenz@79c8f4ef-9946-4ed4-8686-7d53c1674ba1>
2007-08-10 23:50:00 +0000

    New header sort arrows and removed old header bitmaps for owner draw.
    git-svn-id: file:///mnt/d/users/prekageo/desktop/npp-explorer/svn/trunk/NppPlugins/Explorer@244 79c8f4ef-9946-4ed4-8686-7d53c1674ba1

--- src/resource.h
+++ src/resource.h
@@ -1,14 +1,15 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by ExplorerDialog.rc
 //
+
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        163
+#define _APS_NEXT_RESOURCE_VALUE        168
 #define _APS_NEXT_COMMAND_VALUE         40002
 #define _APS_NEXT_CONTROL_VALUE         1036
 #define _APS_NEXT_SYMED_VALUE           108
